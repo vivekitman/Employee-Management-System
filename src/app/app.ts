3e561a3b6f78d3c1c43fdab3beafa64b19c3cce3
Vivek Gupta <vivekitman@gmail.com>
2026-05-16 18:21:45 +0530

    update

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './core/layout/navbar/navbar';
+import { Sidebar } from './core/layout/sidebar/sidebar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Navbar,Sidebar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
